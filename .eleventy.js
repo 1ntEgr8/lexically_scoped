130e3284a4ef868483d9f7f47e8bf0eb3f9e80af
Elton <eltonp3103@gmail.com>
2020-05-21 09:45:08 -0400

    langing page base

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,14 @@
+const CleanCSS = require("clean-css");
+
 module.exports = eleventyConfig => {
+    // filters
+    eleventyConfig.addFilter("cssmin", (code) => {
+        return new CleanCSS({}).minify(code).styles;
+    });
+    
+    // shortcodes
     eleventyConfig.addShortcode("emoji", (emoji, alt="") => {
         return `<span aria-hidden="true" class="emoji">${emoji}</span>` +
 			(alt ? `<span class="sr-only">${alt}</span>` : "");        
-    })
+    });
 }
