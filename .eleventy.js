05883ab6341eec66d8864691bb2660d963837b54
Elton <eltonp3103@gmail.com>
2020-06-22 00:48:45 -0400

    first blog post; syntax highlighting; mathjax; style changes

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,26 @@
 const CleanCSS = require("clean-css");
 const Terser = require("terser");
 const { DateTime } = require("luxon");
+const syntaxHighlightPlugin = require("@11ty/eleventy-plugin-syntaxhighlight");
+const markdownIt = require("markdown-it");
 
 module.exports = eleventyConfig => {
+    eleventyConfig.addPlugin(syntaxHighlightPlugin);
+    
+    let markdownItOptions = {
+        html: true,
+        breaks: true,
+        linkify: true
+    };
+    // markdown
+    eleventyConfig.setLibrary("md", markdownIt(markdownItOptions));
+
     // filters
-    eleventyConfig.addFilter("cssmin", (code) => {
+    eleventyConfig.addFilter("cssmin", code => {
         return new CleanCSS({}).minify(code).styles;
     });
 
-    eleventyConfig.addFilter("jsmin", (code) => {
+    eleventyConfig.addFilter("jsmin", code => {
         let minified = Terser.minify(code);
         if (minified.error) {
             console.log("Terser error: ", minified.error);
@@ -17,20 +29,29 @@ module.exports = eleventyConfig => {
     });
 
     eleventyConfig.addFilter("readableDate", dateObj => {
-        return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
+        return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+            "dd LLL yyyy"
+        );
     });
 
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-        return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+    // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    eleventyConfig.addFilter("htmlDateString", dateObj => {
+        return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+            "yyyy-LL-dd"
+        );
     });
-    
+
     // shortcodes
-    eleventyConfig.addShortcode("emoji", (emoji, alt="") => {
-        return `<span aria-hidden="true" class="emoji">${emoji}</span>` +
-			(alt ? `<span class="sr-only">${alt}</span>` : "");        
+    eleventyConfig.addShortcode("emoji", (emoji, alt = "") => {
+        return (
+            `<span aria-hidden="true" class="emoji">${emoji}</span>` +
+            (alt ? `<span class="sr-only">${alt}</span>` : "")
+        );
     });
 
+    eleventyConfig.addShortcode("anchored-code", (code, link) => {
+        return `<code><a href="${link}" target="_blank">${code}</a></code>`;
+    });
     // passthrough copy
     eleventyConfig.addPassthroughCopy("assets");
-}
+};
